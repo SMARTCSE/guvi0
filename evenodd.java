8473409158e0a54f2c354dacfe3dfff1266c2efa
SMARTCSE <47376568+SMARTCSE@users.noreply.github.com>
2019-03-08 02:17:27 -0800

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+{
+	class Ideone
+//Main
+public class EvenOdd {
+ public static void main(String[] args) {
+ Scanner reader = new Scanner(System.in);
+  int num = reader.nextInt();
+ if(num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
